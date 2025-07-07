924f1e0b6c6a0efd3736b94c1f0dbacbbf87b027
Shivansh Mittal <www.shivansh065@gmail.com>
2025-07-07 10:23:13 +0530

    added

--- React Tutorials/src/App.jsx
+++ React Tutorials/src/App.jsx
@@ -2,17 +2,6 @@ import { useEffect, useState } from "react";
 import useDebounce from "./customHooks/useDebounce";
 
 function App() {
-    const [inputValue, setInputValue] = useState("");
-    const useDebouncedValue = useDebounce(inputValue, 200);
-
-    function change(e) {
-        setInputValue(e.target.value);
-    }
-
-    useEffect(() => {
-        console.log("Expensive Operations");
-    }, [useDebouncedValue]);
-
     return (
         <div>
             <input type="text" onChange={change} />
--- React Tutorials/src/customHooks/useDebounce.js
+++ React Tutorials/src/customHooks/useDebounce.js
@@ -17,3 +17,31 @@ const useDebounce = (value, delay) => {
 };
 
 export default useDebounce;
+
+//----------------------------------------------------------------------------------------------------------------------------------------------
+
+// App.jsx
+
+// import { useEffect, useState } from "react";
+// import useDebounce from "./customHooks/useDebounce";
+
+// function App() {
+//     const [inputValue, setInputValue] = useState("");
+//     const useDebouncedValue = useDebounce(inputValue, 200);
+
+//     function change(e) {
+//         setInputValue(e.target.value);
+//     }
+
+//     useEffect(() => {
+//         console.log("Expensive Operations");
+//     }, [useDebouncedValue]);
+
+//     return (
+//         <div>
+//             <input type="text" onChange={change} />
+//         </div>
+//     );
+// }
+
+// export default App;
